394c88c880aa89a6df8fec31b59937c325218570
Duvan Rondon <robra2406@hotmail.com>
2025-02-20 19:27:38 -0500

    2da clase + mod 01Compute2.tf

--- 01computeEngine-2.tf
+++ 01computeEngine-2.tf
@@ -6,6 +6,8 @@ resource "google_compute_instance" "instancia2" {
     boot_disk {
         initialize_params {
             image = "debian-cloud/debian-11-bullseye-v20250212"
+            size = 50
+            type = "pd-standard"
         }
     }
 
