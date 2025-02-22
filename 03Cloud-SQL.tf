e0ffefe32554c56a6a9928211632295f4a6d22ed
Duvan Rondon <robra2406@hotmail.com>
2025-02-21 19:26:32 -0500

    Add CloudSQL and G Storage

---
+++ 03Cloud-SQL.tf
@@ -0,0 +1,29 @@
+resource "google_sql_database_instance" "sql-cloud-bootcamp" {
+    name = "sql-cloud-bootcamp"
+    database_version = "MYSQL_5_7"
+    region = "us-central1"
+    settings {
+        tier = "db-f1-micro"
+        backup_configuration {
+            enabled = true
+        }
+        ip_configuration {
+            ipv4_enabled = true
+        }
+    }
+
+}
+
+resource "google_sql_user" "sql-cloud-bootcamp-user" {
+    name = "sql-cloud-bootcamp-user"
+    instance = google_sql_database_instance.sql-cloud-bootcamp.name
+    password = "password"
+    host = "%"
+}
+
+resource "google_sql_database" "sql-cloud-bootcamp-db" {
+    name = "sql-cloud-bootcamp-db"
+    instance = google_sql_database_instance.sql-cloud-bootcamp.name
+    charset = "utf8"
+    collation = "utf8_general_ci"
+}
---
+++ 04GStorage.tf
@@ -0,0 +1,26 @@
+resource "google_storage_bucket" "bucket-bootcamp" {
+    name = "bucket-bootcamp-2025-01"
+    location = "us-central1"
+    storage_class = "STANDARD"
+    force_destroy = true
+
+    uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_member" "bucket-bootcamp-iam" {
+    bucket = google_storage_bucket.bucket-bootcamp.name
+    role = "roles/storage.objectViewer"
+    member = "allUsers"
+}
+
+resource "google_storage_bucket_iam_member" "bucket-bootcamp-iam-2" {
+    bucket = google_storage_bucket.bucket-bootcamp.name
+    role = "roles/storage.objectAdmin"
+    member = "user:duvan_andres@consiti.com"
+}
+
+resource "google_storage_bucket_object" "object-bootcamp" {
+    name = "object-bootcamp"
+    bucket = google_storage_bucket.bucket-bootcamp.name
+    source = "index.html"
+}
